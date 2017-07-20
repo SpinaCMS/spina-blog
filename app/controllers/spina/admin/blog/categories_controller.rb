8c66147dff30355a637c6009740909ed926bc1ed
Tom Simnett <tom@initforthe.com>
2017-07-20 14:57:32 +0100

    Better separation of class names so we don't get issues with preloading in production

--- app/controllers/spina/admin/blog/categories_controller.rb
+++ app/controllers/spina/admin/blog/categories_controller.rb
@@ -1,73 +1,75 @@
-class Spina::Admin::Blog::CategoriesController < AdminController
-  before_action :set_breadcrumb
-  before_action :set_locale
+module Spina::Admin
+  class Blog::CategoriesController < AdminController
+    before_action :set_breadcrumb
+    before_action :set_locale
 
-  decorates_assigned :category
+    decorates_assigned :category
 
-  layout 'spina/admin/categories'
+    layout 'spina/admin/categories'
 
-  def index
-    @categories = Spina::Blog::Category.order(:name)
-  end
-
-  def new
-    @category = Spina::Blog::Category.new
-    add_breadcrumb I18n.t('spina.blog.categories.new')
-    render layout: 'spina/admin/admin'
-  end
-
-  def create
-    @category = Spina::Blog::Category.new category_params
-    if @category.save
-      redirect_to spina.edit_admin_blog_category_url(@category.id), notice: t('spina.blog.categories.saved')
-    else
-      add_breadcrumb I18n.t('spina.blog.categories.new')
-      render :new, layout: 'spina/admin/admin'
+    def index
+      @categories = Spina::Blog::Category.order(:name)
     end
-  end
 
-  def edit
-    @category = Spina::Blog::Category.find params[:id]
-    add_breadcrumb @category.name
-    render layout: 'spina/admin/admin'
-  end
+    def new
+      @category = Spina::Blog::Category.new
+      add_breadcrumb I18n.t('spina.blog.categories.new')
+      render layout: 'spina/admin/admin'
+    end
 
-  def update
-    I18n.locale = params[:locale] || I18n.default_locale
-    @category = Spina::Blog::Category.find(params[:id])
-    respond_to do |format|
-      if @category.update_attributes(category_params)
-        add_breadcrumb @category.name
-        @category.touch
-        I18n.locale = I18n.default_locale
-        format.html { redirect_to spina.edit_admin_blog_category_url(@category.id, params: {locale: @locale}), notice: t('spina.blog.categories.saved') }
-        format.js
+    def create
+      @category = Spina::Blog::Category.new category_params
+      if @category.save
+        redirect_to spina.edit_admin_blog_category_url(@category.id), notice: t('spina.blog.categories.saved')
       else
-        format.html do
-          render :edit, layout: 'spina/admin/admin'
+        add_breadcrumb I18n.t('spina.blog.categories.new')
+        render :new, layout: 'spina/admin/admin'
+      end
+    end
+
+    def edit
+      @category = Spina::Blog::Category.find params[:id]
+      add_breadcrumb @category.name
+      render layout: 'spina/admin/admin'
+    end
+
+    def update
+      I18n.locale = params[:locale] || I18n.default_locale
+      @category = Spina::Blog::Category.find(params[:id])
+      respond_to do |format|
+        if @category.update_attributes(category_params)
+          add_breadcrumb @category.name
+          @category.touch
+          I18n.locale = I18n.default_locale
+          format.html { redirect_to spina.edit_admin_blog_category_url(@category.id, params: {locale: @locale}), notice: t('spina.blog.categories.saved') }
+          format.js
+        else
+          format.html do
+            render :edit, layout: 'spina/admin/admin'
+          end
         end
       end
     end
+
+    def destroy
+      @category = Spina::Blog::Category.find(params[:id])
+      @category.destroy
+      redirect_to spina.admin_blog_categories_path
+    end
+
+    private
+
+    def set_breadcrumb
+      add_breadcrumb I18n.t('spina.blog.categories.name'), spina.admin_blog_categories_path
+    end
+
+    def set_locale
+      @locale = params[:locale] || I18n.default_locale
+    end
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
+
   end
-
-  def destroy
-    @category = Spina::Blog::Category.find(params[:id])
-    @category.destroy
-    redirect_to spina.admin_blog_categories_path
-  end
-
-  private
-
-  def set_breadcrumb
-    add_breadcrumb I18n.t('spina.blog.categories.name'), spina.admin_blog_categories_path
-  end
-
-  def set_locale
-    @locale = params[:locale] || I18n.default_locale
-  end
-
-  def category_params
-    params.require(:category).permit(:name)
-  end
-
 end
