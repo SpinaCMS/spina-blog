c8db8a73a62667b72e03aa8809a795d98491e34d
Tomislav Simnett <tom@initforthe.com>
2020-06-07 09:53:00 +0100

    Add read time methods to the decorator

--- app/decorators/spina/blog/post_decorator.rb
+++ app/decorators/spina/blog/post_decorator.rb
@@ -4,8 +4,22 @@ module Spina
   module Blog
     # Spina::Blog::PostDecorator
     class PostDecorator < Draper::Decorator
+      delegate_all
+
       def published_date
-        l model.published_at, format: :long
+        l model.published_at.to_date, format: :long
+      end
+
+      def word_count
+        h.strip_tags(content).split.size
+      end
+
+      def image_count
+        Nokogiri::HTML(content).css('img').size
+      end
+
+      def read_time
+        ((word_count / (275 / 60.to_f) + (image_count * 12)) / 60).ceil
       end
     end
   end
