2e9710a82c9c640a4be48bc6d3ab961db19e6a12
Dirk Nitschke <nitschke.dirk+github@gmail.com>
2022-03-08 00:09:14 +0100

    Add user factory, use association in posts factory

--- spec/factories/spina/blog/posts.rb
+++ spec/factories/spina/blog/posts.rb
@@ -5,12 +5,16 @@ FactoryBot.define do
     sequence(:title) { |n| "Blog Post #{n}" }
     content { 'Some content for my post' }
     association :category, factory: :spina_blog_category
+    association :user, factory: :spina_user
 
     seo_title { 'Some title for SEO' }
     description { 'Some description for SEO' }
 
     factory :invalid_spina_blog_post do
-      title { nil }
+      title       { nil }
+      content     { nil }
+      user_id     { nil }
+      category_id { nil }
     end
   end
 end
