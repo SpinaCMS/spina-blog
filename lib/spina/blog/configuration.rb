f4a41dfcc83951a948c1bfb4c5cd5f1e1735c388
aseroff <aseroff@umn.edu>
2025-10-29 14:47:23 -0500

    Update configuration.rb

--- lib/spina/blog/configuration.rb
+++ lib/spina/blog/configuration.rb
@@ -4,7 +4,7 @@ module Spina
   # Spina::Blog
   module Blog
     mattr_accessor :title, default: 'Blog'
-    mattr_accessor :controller, default: 'blog',
+    mattr_accessor :controller, default: 'blog'
     mattr_accessor :description, default: 'Blog posts'
     mattr_accessor :spina_icon, default: 'pencil-outline'
     mattr_accessor :plugin_type, default: 'website_resource'
